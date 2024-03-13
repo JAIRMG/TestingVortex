ce6c84ffa8024d2556d87851bb841a330699d9d2
Jair Moreno Gaspar <jair.moreno-gaspar@flixbus.com>
2024-03-13 16:03:13 +0100

    Initial Commit

---
+++ TestingVortex/TestingVortexApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingVortexApp.swift
+//  TestingVortex
+//
+//  Created by Jair Moreno Gaspar on 13.03.24.
+//
+
+import SwiftUI
+
+@main
+struct TestingVortexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
